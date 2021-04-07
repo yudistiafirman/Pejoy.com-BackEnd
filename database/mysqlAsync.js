fb206d35f91e2c53802985b584834c14cd1c8e2a
Firman Hadi <Yudistia.firman@gmail.com>
2021-04-07 09:36:15 +0700

    firman

--- database/mysqlAsync.js
+++ database/mysqlAsync.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 require('dotenv').config()
 
 const db = mysql.createConnection({
-  
+    host:'localhost',
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
-    port :process.env.DB_PORT,
+
     database : process.env.DB_NAME
 })
 
